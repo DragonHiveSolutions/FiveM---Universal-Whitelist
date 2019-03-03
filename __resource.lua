be33676537f0e66adfb837a75728b8384763a288
Kye Jones <kyej1023@gmail.com>
2019-03-03 23:33:00 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+server_script 'versioncheck.lua'
 server_script 'config.lua'
 server_script '@mysql-async/lib/MySQL.lua'
 server_script 'server.lua'
